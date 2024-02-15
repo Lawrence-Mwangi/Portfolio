ad5d01caa9e60520989f1910499a99ffc6ecfe44
Lawrence-Mwangi <lawrencemwangi082@gmail.com>
2024-02-15 22:13:22 +0300

    Add a blog section for the portfolio

--- src/views/BlogView.vue
+++ src/views/BlogView.vue
@@ -1,12 +1,48 @@
 <script setup></script>
 
 <template>
-   <section class="Blog">
+  <section class="Blog">
     <h1>Blog</h1>
     <div class="blog_container">
-       <div class="blog_content">
-        <span></span>
-       </div>
+        <div class="blog_content">
+            <span>&lt;Web Design&gt;</span>
+            <p>Web design is the process of creating websites that are visually appealing, 
+                easy to navigate, and function smoothly. It involves various elements such as layout,
+                color schemes, typography, and graphics.
+            </p>
+            <p>
+                User interface (UI) design focuses on how the website looks and feels. It involves arranging elements
+                on the page in a way that is aesthetically pleasing and intuitive for users to interact with. 
+                This includes designing buttons, menus, forms, and other interactive components.
+            </p>
+        </div>
+        <div class="blog_content">
+            <span>&lt;Music&gt;</span>
+            <p>
+                Music, an art form that transcends boundaries, speaks volumes in mere notes. From symphonies
+                 echoing through centuries to catchy tunes on the radio, it intertwines with our lives, stirring 
+                 emotions and memories. 
+            </p>
+                Whether it's the rhythm that moves our feet or lyrics that touch our souls,
+                music unites us in its universal language. Explore the melodies that shape our world and discover
+                the endless symphony of human expression."
+            <p>
+
+            </p>
+        </div>
+        <div class="blog_content">
+            <span>&lt;Programming &gt;</span>
+            <p>
+                Programming is the art of instructing computers to perform tasks through carefully crafted sets of 
+                instructions, known as code.
+            </p>
+            <p>
+                It encompasses a wide array of languages, each with its own syntax and purpose, 
+                from web development with languages like HTML, CSS, and JavaScript, to software engineering with 
+                languages such as Python, Java, and C++. Programming empowers individuals to create software, automate
+                processes, and solve complex problems in innovative ways.
+            </p>
+        </div>
     </div>
-   </section>
+  </section>
 </template>
